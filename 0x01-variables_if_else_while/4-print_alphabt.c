6442e649c5f9cb8f95cb4e68a346b3d309cbbb52
Krissy22 <chettykriselda@gmail.com>
2023-02-17 14:21:16 -0800

    the 4th ABC

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Prints the lowercase alphabet except for q & e
+ *
+ * Return: (0) successful execution
+ */
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; ++letters)
+	{
+		if (letters != 'q' && letters != 'e')
+		{
+			putchar(letters);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
