9f9884931eb331349d5e264fb6f627c9e03fae84
Krissy22 <chettykriselda@gmail.com>
2023-02-17 14:15:49 -0800

    THE ABCS

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Use 'putchar' to print the lowercase alphabet with a newline
+ *
+ * Return: (0) successful execution
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
